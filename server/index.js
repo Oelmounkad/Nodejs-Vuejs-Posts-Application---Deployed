bf44a85c64e613ccf7c0066167dbb2cc4d0fc36d
Elmounkad <oelmounkad@gmail.com>
2020-07-01 23:11:50 +0200

    added static folder and prepared for deployment

--- server/index.js
+++ server/index.js
@@ -13,6 +13,20 @@ const posts = require('./routes/api/posts')
 
 app.use('/api/posts',posts)
 
+
+//Handle production
+
+if(process.env.NODE_ENV === 'production'){
+
+// Static folder
+app.use(express.static(__dirname + '/public'));
+
+// Handle SPA
+
+app.get(/.*/, (req,res) => res.sendFile(__dirname, '/public/index.html'));
+
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
